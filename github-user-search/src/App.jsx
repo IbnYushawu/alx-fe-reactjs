bb7156567a583fdd71fcc54d52327335f36d8442
Ayuba Yushawu <iyushawu@gmail.com>
2026-01-30 22:25:37 +0000

    Added github-user-search directory

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>GitHub User Search</h1>
+      <p>Search for GitHub profiles using the GitHub API</p>
+    </div>
+  );
+}
+
+export default App;
