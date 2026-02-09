f07ea65dc24f5b136d8fe218c5ba5d5b25d487b0
Ayuba Yushawu <iyushawu@gmail.com>
2026-02-09 23:03:11 +0000

    Recipe sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-green-600">
+        Tailwind is working ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
