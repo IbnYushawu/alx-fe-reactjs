f7726854c48e58633f8bcbc7828828daad758b1b
Ayuba Yushawu <iyushawu@gmail.com>
2026-02-18 10:30:04 +0000

    Updated App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <PostsComponent />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </>
+    </QueryClientProvider>
   );
 }
 
